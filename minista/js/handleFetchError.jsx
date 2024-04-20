faed4738f4053a138324a1f7a201325156ec5c88
hyunkyu-Park <park620630@gmail.com>
2024-04-19 23:33:09 -0400

    updating error handling

--- minista/js/handleFetchError.jsx
+++ minista/js/handleFetchError.jsx
@@ -1,9 +1,17 @@
 const handleFetchError = (error) => {
-    if (error.status === 404) {
-        alert("Page Not Found");
+    if (error.message == "BAD REQUEST") {
+        alert("Bad Request");
     } else if (error.status === 400) {
         alert("Bad Request");
     } else {
+        console.error(error);
+    }
+    if(error.message == "CONFLICT"){
+        alert("existing username");
+        // reset the username inside of inputs in form
+        event.target.elements.username.value = '';
+    }
+    else{
         console.error(error);
     }
 };
