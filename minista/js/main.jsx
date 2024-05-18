33b9fc8afe62009b3ae95c0b934037ccb5b783a6
hyunkyu-Park <park620630@gmail.com>
2024-05-18 13:41:23 -0400

    change logo & update dark mode

--- minista/js/main.jsx
+++ minista/js/main.jsx
@@ -1,83 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
-import Post from "./post";
-import '../static/css/style.css'; 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Post from "./post";
 import PostDetail from "./post_detail";
 import UserPage from "./userPage";
-import UserEditPage from "./userEditPage"
-import ChangePassword from "./changePasswordPage"
-import DeleteAccount from "./deleteAccount"
-import Followers from "./Followers"
-import Following from "./Following"
+import UserEditPage from "./userEditPage";
+import ChangePassword from "./changePasswordPage";
+import DeleteAccount from "./deleteAccount";
+import Followers from "./Followers";
+import Following from "./Following";
 import Explore from "./Explore";
-import LoginPage from "./Login"
+import LoginPage from "./Login";
 import CreatePage from "./Create";
+import '../static/css/style.css'; 
 
 // Create a root
 const root = createRoot(document.getElementById("reactEntry"));
 
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  // check if the user previously selected dark mode
+  useEffect(() => {
+    const savedMode = localStorage.getItem('darkMode');
+    if (savedMode === 'enabled') {
+      setDarkMode(true);
+      document.body.classList.add('dark-mode');
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    if (darkMode) {
+      setDarkMode(false);
+      document.body.classList.remove('dark-mode');
+      localStorage.removeItem('darkMode');
+    } else {
+      setDarkMode(true);
+      document.body.classList.add('dark-mode');
+      localStorage.setItem('darkMode', 'enabled');
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={toggleDarkMode} style={{ marginTop: 10 }}>
+        {darkMode ? 'Light Mode' : 'Dark Mode'}
+      </button>
+
+      <Routes>
+        <Route path="/" element={<Post url="/api/v1/posts/" />} />
+        <Route path="/posts/:id/" element={<PostDetail />} />
+        <Route path="/users/:username/" element={<UserPage />} />
+        <Route path="/accounts/edit/" element={<UserEditPage />} />
+        <Route path="/accounts/password/" element={<ChangePassword />} />
+        <Route path="/accounts/delete/" element={<DeleteAccount />} />
+        <Route path="/users/:username/followers" element={<Followers />} />
+        <Route path="/users/:username/following" element={<Following />} />
+        <Route path="/explore/" element={<Explore />} />
+        <Route path="/accounts/login/" element={<LoginPage />} />
+        <Route path="/accounts/create/" element={<CreatePage />} />
+      </Routes>
+    </div>
+  );
+}
 
 root.render(
   <BrowserRouter>
-    <div>
-      <Routes>
-        <Route 
-          path="/"
-          element={<Post url="/api/v1/posts/"/>}
-        />
-
-        <Route
-          path="/posts/:id/"
-          element={<PostDetail />}
-        />
-
-        <Route 
-          path="/users/:username/" 
-          element={<UserPage />}
-        />
-
-        <Route
-          path="/accounts/edit/"
-          element={<UserEditPage/>}
-        />
-
-        <Route
-          path="/accounts/password/"
-          element={<ChangePassword/>}
-        />
-
-        <Route
-          path="/accounts/delete/"
-          element={<DeleteAccount/>}
-        />
-
-        <Route 
-          path="/users/:username/followers" 
-          element={<Followers />}
-        />
-
-        <Route 
-          path="/users/:username/following" 
-          element={<Following />}
-        />
-
-        <Route
-          path="/explore/"
-          element={<Explore/>}
-        />
-
-        <Route
-          path="/accounts/login/"
-          element={<LoginPage/>}
-        />
-
-        <Route
-          path="/accounts/create/"
-          element={<CreatePage/>}
-        />
-
-      </Routes>
-    </div>
+    <App />
   </BrowserRouter>
 );
